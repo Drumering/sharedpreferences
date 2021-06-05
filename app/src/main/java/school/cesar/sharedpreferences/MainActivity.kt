9412c9a01924472263f82c59d81d511b2aa0fbd7
Anderson Mendes de Almeida <ama@cesar.school>
2021-06-05 13:56:14 -0300

    [TASK]Created shared pref screen with toasts

--- app/src/main/java/school/cesar/sharedpreferences/MainActivity.kt
+++ app/src/main/java/school/cesar/sharedpreferences/MainActivity.kt
@@ -4,10 +4,15 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
+import androidx.preference.CheckBoxPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import school.cesar.sharedpreferences.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     private lateinit var binding : ActivityMainBinding
 
@@ -16,20 +21,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val prefs: SharedPreferences = getSharedPreferences("none",Context.MODE_PRIVATE)
-        val editor = prefs.edit()
+    }
 
-        binding.btnEscrever.setOnClickListener {
-            editor.apply {
-                putString("name", binding.etName.text.toString())
-                putString("age", binding.etIdade.text.toString())
-                apply()
+    override fun onResume() {
+        super.onResume()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.pref_screen, rootKey)
+            val cbPref = findPreference<CheckBoxPreference>("preference_key")
+            cbPref?.let {
+                it.onPreferenceChangeListener = this
             }
         }
 
-        binding.btnLer.setOnClickListener {
-            binding.tvResult.text = "${prefs.getString("name", "Placeholder")}, sua idade é ${prefs.getString("age", "84")}"
+        override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
+            Toast.makeText(context, "Value $newValue", Toast.LENGTH_SHORT).show()
+            return true
         }
+    }
 
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        Toast.makeText(this, "Pref: $sharedPreferences - Value: ${sharedPreferences?.getBoolean(key, false)}", Toast.LENGTH_SHORT).show()
     }
 }
